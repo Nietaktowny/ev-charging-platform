ac45797d79ed5a6ad2fc2177105ebf10a20cd2c5
Nietaktowny <wojciech.lukasz.mytych@gmail.com>
2026-03-10 07:07:24 +0100

    Added mysql resource

--- src/definitions.py
+++ src/definitions.py
@@ -1,6 +1,22 @@
 from dagster import Definitions
 from src.assets.ingest import kaggle_raw_ev_charging_dataset
+from src.assets.bronze import ev_data_landing
+from src.resources.mysql import MySQLResource
+import os
+
+assets = [kaggle_raw_ev_charging_dataset, ev_data_landing]
+
+resources = {
+        "mysql": MySQLResource(
+            host=os.getenv("MYSQL_HOST", "mysql"),
+            port=int(os.getenv("MYSQL_PORT", "3306")),
+            database=os.getenv("MYSQL_DB", "ev_platform"),
+            user=os.getenv("MYSQL_USER", "ev_user"),
+            password=os.getenv("MYSQL_PASSWORD", "ev_pass"),
+        )
+    }
 
 defs = Definitions(
-    assets=[kaggle_raw_ev_charging_dataset],
+    assets=assets,
+    resources=resources,
 )
---
+++ src/resources/mysql.py
@@ -0,0 +1,16 @@
+from dagster import ConfigurableResource
+from sqlalchemy import create_engine
+
+
+class MySQLResource(ConfigurableResource):
+    host: str
+    port: int
+    database: str
+    user: str
+    password: str
+
+    def get_engine(self):
+        return create_engine(
+            f"mysql+pymysql://{self.user}:{self.password}@{self.host}:{self.port}/{self.database}",
+            pool_pre_ping=True,
+        )
