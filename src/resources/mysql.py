a39469bd1af9e4a8a231d8caf4273c30d332025a
Nietaktowny <wojciech.lukasz.mytych@gmail.com>
2026-03-11 18:11:24 +0100

    Enable local-infile for MySQL resource
    Allow the use of LOAD DATA LOCAL INFILE statements by passing the local_infile argument to the SQLAlchemy engine connection.

--- src/resources/mysql.py
+++ src/resources/mysql.py
@@ -13,4 +13,5 @@ class MySQLResource(ConfigurableResource):
         return create_engine(
             f"mysql+pymysql://{self.user}:{self.password}@{self.host}:{self.port}/{self.database}",
             pool_pre_ping=True,
+            connect_args={"local_infile": True}
         )
